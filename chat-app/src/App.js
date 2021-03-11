d112c9ef4f047aff57a77aa1abdc14c11d8735a3
han nguyen <hannguyencit@gmail.com>
2021-03-11 09:04:09 +0700

    update

---
+++ chat-app/src/App.js
@@ -0,0 +1,14 @@
+import { ChatEngine } from 'react-chat-engine';
+import './App.css';
+
+const App =()=>{
+  return (
+    <ChatEngine
+        height="100vh"
+        projectID="2329c652-bf73-48eb-9fb8-06404eafa1c8"
+        userName="han@gmail.com"
+        userSecret="123123"
+    />
+  );
+}
+export default App; 
